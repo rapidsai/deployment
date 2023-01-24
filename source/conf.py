df7f03891fb4e26e6cb56766dc6f113e797866bf
Jacob Tomlinson <jtomlinson@nvidia.com>
2023-01-24 14:58:16 +0000

    Enable flake8 rules

--- source/conf.py
+++ source/conf.py
@@ -10,11 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import datetime
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../extensions"))
-import datetime
 
 # -- Project information -----------------------------------------------------
 
