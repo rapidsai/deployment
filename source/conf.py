3054f6e478a73df1affc3ce1a44c4965a9b7173b
Jacob Tomlinson <jtomlinson@nvidia.com>
2023-08-25 14:05:57 +0100

    Enable dollarmath

--- source/conf.py
+++ source/conf.py
@@ -72,7 +72,7 @@ extensions = [
     "rapids_admonitions",
 ]
 
-myst_enable_extensions = ["colon_fence"]
+myst_enable_extensions = ["colon_fence", "dollarmath"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
