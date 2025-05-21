ddf79e549416aeeb3e9b9e07eb2bf2b757974637
Jacob Tomlinson <jtomlinson@nvidia.com>
2025-05-21 15:11:44 +0100

    Restrict unused images pre-commit hook to docs source

--- scripts/unused_images.sh
+++ scripts/unused_images.sh
@@ -2,12 +2,12 @@
 
 set -e -u -o pipefail
 
-imagepaths=$(find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.svg" \) ! -path "./build/*" ! -path "./node_modules/*" ! -path "./jupyter_execute/*" ! -path "./.ipynb_checkpoints/*")
+imagepaths=$(find source -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.svg" \) ! -path "./build/*" ! -path "./node_modules/*" ! -path "./jupyter_execute/*" ! -path "./.ipynb_checkpoints/*")
 counter=0
 
 for imagepath in $imagepaths; do
     filename=$(basename -- "$imagepath")
-    if ! grep -q -r --exclude-dir=".git" --exclude-dir="build" --exclude-dir="node_modules" --exclude-dir="jupyter_execute" --exclude-dir=".ipynb_checkpoints" "$filename" .; then
+    if ! grep -q -r --exclude-dir=".git" --exclude-dir="build" --exclude-dir="node_modules" --exclude-dir="jupyter_execute" --exclude-dir=".ipynb_checkpoints" "$filename" source README.md; then
         echo "Found unused image $imagepath"
         counter=$((counter+1))
     fi
